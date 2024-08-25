7ae19ace0eb47483a6aba71ebea2b4104f6126df
steeve0403 <zychsteeve4@gmail.com>
2024-08-25 19:22:04 +0200

    exercice : sql request

--- SQL/create_sqlite.py
+++ SQL/create_sqlite.py
@@ -10,5 +10,24 @@ CREATE TABLE artist (
     );
 """)
 
+cursor.execute("""
+    CREATE TABLE album (
+        album_id INTEGER NOT NULL PRIMARY KEY,
+        artist_id INTEGER REFERENCES artist,
+        title VARCHAR,
+        release_date INTEGER
+    );
+""")
+
+cursor.execute(f'INSERT INTO artist (name) VALUES ("Mickael Jackson");')
+mj_id = cursor.lastrowid
+cursor.execute(f'INSERT INTO artist (name) VALUES ("Céline Dion");')
+cd_id = cursor.lastrowid
+
+cursor.execute(f'INSERT INTO album (artist_id, title, release_date) VALUES ({str(mj_id)}, "Thriller", 1983);')
+cursor.execute(f'INSERT INTO album (artist_id, title, release_date) VALUES ({str(cd_id)}, "Falling into you", 1996);')
+cursor.execute(f'INSERT INTO album (artist_id, title, release_date) VALUES ({str(cd_id)}, "Let\'s talk about love", 1997);')
+
+
 connection.commit()
 connection.close()
