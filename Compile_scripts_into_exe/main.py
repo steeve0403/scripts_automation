adb8a8777c48e03d3c7cc588f3515db86eed222a
steeve0403 <zychsteeve4@gmail.com>
2024-08-27 10:56:21 +0200

    share file on browser

--- Compile_scripts_into_exe/main.py
+++ Compile_scripts_into_exe/main.py
@@ -1,3 +1,16 @@
 name = input("What is your name? ")
 
 print(f"Hello {name}")
+
+# ---- Commands Mac for .exe ----
+
+# pip install PyInstaller
+# cd
+# python main.py (or file to .exe)
+
+# ---- Other commands
+# delete directory and add command : pyinstaller --onefile main.py (or file to .exe)
+# Ony file to .exe
+
+# zip -> directory
+
